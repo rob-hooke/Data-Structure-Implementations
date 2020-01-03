712a9ba956c62440299dc2dcd91971e041822bdb
Boby Robert <bobyrobert3@gmail.com>
2020-01-03 12:21:30 +0530

    Dijkstra's Algorithm ( Adjanceny Matrix)
    Shortest paths from source to all vertices in the given graph.

---
+++ Graph/Djikstra_AdjacencyMatrix.py
@@ -0,0 +1,49 @@
+def getClosest(distance,visited,n):
+
+    minDist = float('inf') - 1
+    minDistIndex = 0
+
+    for i in range(n):
+        if distance[i] < minDist and not visited[i]:
+            minDist = distance[i]
+            minDistIndex = i
+
+    return minDistIndex
+            
+def djikstra(adj_matrix,src):
+
+    n = len(adj_matrix)
+    visited = [False] * n
+
+    distance = [float('inf')] * n
+
+    distance[src] = 0
+
+
+    for i in range(n):
+        
+        nearest =  getClosest(distance,visited,n)
+        print(distance,nearest,end='\n')
+        visited[nearest] = True
+
+        for j in range(n):
+            if adj_matrix[nearest][j] > 0 and not visited[j]  and distance[j] > distance[nearest] + adj_matrix[nearest][j]:
+                distance[j] = distance[nearest] + adj_matrix[nearest][j]
+
+    print(distance,end='\n')
+            
+        
+if __name__ == '__main__':
+    
+    adj_matrix = [  [0, 4, 0, 0, 0, 0, 0, 8, 0], 
+                    [4, 0, 8, 0, 0, 0, 0, 11, 0], 
+                    [0, 8, 0, 7, 0, 4, 0, 0, 2], 
+                    [0, 0, 7, 0, 9, 14, 0, 0, 0], 
+                    [0, 0, 0, 9, 0, 10, 0, 0, 0], 
+                    [0, 0, 4, 14, 10, 0, 2, 0, 0], 
+                    [0, 0, 0, 0, 0, 2, 0, 1, 6], 
+                    [8, 11, 0, 0, 0, 0, 1, 0, 7], 
+                    [0, 0, 2, 0, 0, 0, 6, 7, 0] 
+                 ]
+
+    djikstra(adj_matrix,0)
